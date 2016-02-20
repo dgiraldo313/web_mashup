adaeaa9daa9872b5baaa4f5061e6a7da5ab294ad
Wyatt Olney <wolney@drew.edu>
2016-02-20 16:10:12 -0500

    Merge branch 'master' into wyatt-testing
    # Conflicts:
    #	Gemfile

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'bootstrap-sass'
+
 gem 'simplecov', :require => false, :group => :test
 
 gem 'responders', '~> 2.0'
---
+++ features/step_definitions/search.rb
@@ -0,0 +1,23 @@
+Given(/^a set of options to search for in the page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I enter the fields into the page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I will retrieve an XML document$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^an XML doc$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the doc is returned$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I will see the links to resources automatically$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
