46128edea3cdb078222d651546949dfe409277d4
jegash <jmuneeswaran@drew.edu>
2016-04-06 06:48:39 -0400

    commit to search_query.rb

--- app/controllers/api/v0/search_queries_controller.rb
+++ app/controllers/api/v0/search_queries_controller.rb
@@ -101,6 +101,35 @@ class Api::V0::SearchQueriesController < ApplicationController
      url = nil
     end
 
+    #add DPLA content to hash
+   count = data_hash["count"]
+   dpla_hash= @result_hash[:DPLA]= {:count=>count}
+   if count> 10
+     count = 10
+   end
+   if count > 0
+     for i in 0..(count-1)
+       begin
+         title = data_hash["docs"][i]["sourceResource"]["title"]
+         creator = data_hash["docs"][i]["sourceResource"]["creator"]
+         pub_date = data_hash["docs"][i]["sourceResource"]["date"]["end"]
+         provider = data_hash["docs"][i]["provider"]["name"]
+         publisher = data_hash["docs"][i]["sourceResource"]["publisher"]
+         url = data_hash["docs"][i]["isShownAt"]
+         begin
+           city = data_hash["docs"][i]["sourceResource"]["spatial"]["city"]
+           country = data_hash["docs"][i]["sourceResource"]["spatial"]["country"]
+           location = city + ", " + country
+           dpla_hash[i]= {:title=>title, :author => creator, :pub_date=> pub_date, :provider=> provider, :publisher=> publisher, :location=> location, :url => url}
+         rescue Exception => e
+           dpla_hash[i]= {:title=>title, :author => creator, :pub_date=> pub_date, :provider=> provider, :publisher=> publisher, :url => url}
+         end
+       rescue
+         url = nil
+       end
+     end
+   end
+
     #build url to send request to api (Ex. api.dpla.com/?title....)
     # figure out how to make GET request with ruby
       #http://docs.ruby-lang.org/en/2.0.0/Net/HTTP.html this looks like a good resoruce to figure out get requests on ruby
@@ -110,4 +139,57 @@ class Api::V0::SearchQueriesController < ApplicationController
 
     return url
   end
+end
+
+#Jega
+#This is the function that will create the get request to
+# retrieve the url through the HathiTrust API.
+# We would need to make a GET request to the API.
+# Configure together the url by using the parameters passed in to the function( author, title, etc..)
+# this function should return a string with the url to the microfiche or null if nothing found.
+
+def get_HathiTrust_url(title, author, start_pub_year, end_pub_year)
+  search_prep(title, author, start_pub_year, end_pub_year)
+  base_url = 'https://www.hathitrust.org/data_api'
+  search_url = '/v2/items?'
+  search_url += ('sourceResource.title=' + @new_title + '&sourceResource.creator=' + @new_author + '&sourceResource.date.after=' + @start_date + '&sourceResource.date.before=' + @stop_date + '&api_key=' + @api_key )
+  final_url = base_url + search_url
+  final_url_uri = URI.parse(final_url)
+  response = Net::HTTP.get_response(final_url_uri)
+  response_body = response.body
+  data_hash = JSON.parse(response_body)
+  #json_data = Net::HTTP.get(URI.parse(search_url))
+  #file = file.read(json_data)
+  #data_hash = JSON.parse(json_data)
+
+  #add HathiTrust content to hash
+  count = data_hash["count"]
+  dpla_hash= @result_hash[:HathiTrust]= {:count=>count}
+  if count> 10
+    count = 10
+  end
+  if count > 0
+    for i in 0..(count-1)
+      begin
+        title = data_hash["docs"][i]["sourceResource"]["title"]
+        creator = data_hash["docs"][i]["sourceResource"]["creator"]
+        pub_date = data_hash["docs"][i]["sourceResource"]["date"]["end"]
+        provider = data_hash["docs"][i]["provider"]["name"]
+        publisher = data_hash["docs"][i]["sourceResource"]["publisher"]
+        url = data_hash["docs"][i]["isShownAt"]
+        begin
+          city = data_hash["docs"][i]["sourceResource"]["spatial"]["city"]
+          country = data_hash["docs"][i]["sourceResource"]["spatial"]["country"]
+          location = city + ", " + country
+          hathitrust_hash[i]= {:title=>title, :author => creator, :pub_date=> pub_date, :provider=> provider, :publisher=> publisher, :location=> location, :url => url}
+        rescue Exception => e
+          hathitrust_hash[i]= {:title=>title, :author => creator, :pub_date=> pub_date, :provider=> provider, :publisher=> publisher, :url => url}
+        end
+      rescue
+        url = nil
+      end
+    end
+  end
+  return url
+end
 end
