58ab687a4e099deb25e9cd9e68ce045660183bd3
jegash <jmuneeswaran@drew.edu>
2016-04-05 20:52:30 -0400

    APi- Search Queries

--- app/controllers/api/v0/search_queries_controller.rb
+++ app/controllers/api/v0/search_queries_controller.rb
@@ -167,3 +167,53 @@ class Api::V0::SearchQueriesController < ApplicationController
     return url
   end
 end
+
+#Jega
+#This is the function that will create the get request to
+# retrieve the url through the HathiTrust API.
+# We would need to make a GET request to the API.
+# Configure together the url by using the parameters passed in to the function( author, title, etc..)
+# this function should return a string with the url to the microfiche or null if nothing found.
+
+def get_HathiTrust_url(title, author, start_pub_year, end_pub_year)
+  search_prep(title, author, start_pub_year, end_pub_year)
+  base_url = 'https://babel.hathitrust.org'
+  search_url = 'pt?id='
+  search_url += ('sourceResource.title=' + @new_title + '&sourceResource.creator=' + @new_author + '&sourceResource.date.after=' + @start_date + '&sourceResource.date.before=' + @stop_date + '&api_key=' + @api_key )
+  final_url = base_url + search_url
+  final_url_uri = URI.parse(final_url)
+  response = Net::HTTP.get_response(final_url_uri)
+  response_body = response.body
+  data_hash = JSON.parse(response_body)
+  #json_data = Net::HTTP.get(URI.parse(search_url))
+  #file = file.read(json_data)
+  #data_hash = JSON.parse(json_data)
+
+  #add HathiTrust content to hash
+  count = data_hash["count"]
+  dpla_hash= @result_hash[:HathiTrust]= {:count=>count}
+  if count> 10
+    count = 10
+  end
+  if count > 0
+    for i in 0..(count-1)
+      begin
+        title = data_hash["docs"][i]["sourceResource"]["title"]
+        creator = data_hash["docs"][i]["sourceResource"]["creator"]
+        pub_date = data_hash["docs"][i]["sourceResource"]["date"]["end"]
+        provider = data_hash["docs"][i]["provider"]["name"]
+        publisher = data_hash["docs"][i]["sourceResource"]["publisher"]
+        url = data_hash["docs"][i]["isShownAt"]
+        begin
+          city = data_hash["docs"][i]["sourceResource"]["spatial"]["city"]
+          country = data_hash["docs"][i]["sourceResource"]["spatial"]["country"]
+          location = city + ", " + country
+          hathitrust_hash[i]= {:title=>title, :author => creator, :pub_date=> pub_date, :provider=> provider, :publisher=> publisher, :location=> location, :url => url}
+        rescue Exception => e
+          hathitrust_hash[i]= {:title=>title, :author => creator, :pub_date=> pub_date, :provider=> provider, :publisher=> publisher, :url => url}
+        end
+      rescue
+        url = nil
+      end
+    end
+  end
--- features/step_definitions/SearchForm.rb
+++ features/step_definitions/SearchForm.rb
@@ -3,58 +3,74 @@ Given(/^I am on the home page$/) do
 end
 
 Then(/^I should see "([^"]*)"$/) do |text|
-  page.should have_content: path
+  config.expect_content = true
+  page.should have_content:'Signup'
 end
 
 Then(/^I should see  "([^"]*)"$/) do |path|
+  expected_content = /Search/
   page.should have_button, content: path
 end
 
 Then(/^I should input "([^"]*)"$/) do |text|
-  #@message = @ personal_details.text
+  expected_content = /Personal_details/
+  page.should have_field, content: path
 end
 
 Given(/^I am on the Signup page$/) do
-  #@messages = @Signup.link
-  #should_not_continue = true if  Signup_link.failed
+  visit ('/')
+  messages = @Signup.link
+  should_not_continue = true if  Signup_link.failed
 end
 
 When(/^I see  Signup link$/) do |message|
-  #@message.should inlcude(message)
+  visit('/Signup_link')
+  message.should inlcude: text
 end
 
 When(/^I should click$/) do |message|
+  visit('/click')
   #pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I should see input information page$/) do
+  expected_content = /information_page/
+  page.should have_information, content: path
   #pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I should type personal information$/) do
+  expected_content = /personal_information/
+  page.should have_information, content: path
   #pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given(/^I am on the Search page$/) do
-  visit '/'
+  visit ('/')
 end
 
 When(/^I see "([^"]*)"$/) do |arg1|
+  visit('Title')
   #name, with: text
 end
 
 When(/^I type "([^"]*)"$/) do |arg1|
+  visit('Ruby_on_rails')
   #type_Author element
 end
 
 Given(/^I am on the Hathi Trust result page$/) do
-  visit '/'
+  visit('/')
+  messages = HathiTrust.page
+  should_not_continue = true if  Signup_link.failed
 end
 
 When(/^I see relevant search results$/) do |arg1|
-#page.content : display
+  visit('revelant_search')
+  #page.content : display
 end
 
 Then(/^I should see numbers of results displayed$/) do
-#page.should not_appear: path
+  expected_content = /number_of_results/
+  page.should not_appear: path
 end
